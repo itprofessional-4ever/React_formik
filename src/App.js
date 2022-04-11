6b82c4f67100509fda6b1186aeb6ec1ed114e361
Chloe <chloee@ChloeuicBookAir.cg.shawcable.net>
2022-04-11 00:52:33 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { RegisterForm } from './RegisterForm'
+
+function App () {
+  return (
+    <div className='app'>
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export default App
